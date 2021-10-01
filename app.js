bfac21d1544f674e262497cbc7ed10f686682403
Francisco Santos <santos.frau@gmail.com>
2021-09-30 22:36:19 -0300

    Criação de método log()

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+var moment = require('moment');
 
 const port = 3000;
 
 let server = http.createServer();
 server.listen(port, () => {
-    console.log(`Server está executando na porta ${port}`);
+    log(`Server está executando na porta ${port}`);
 });
 
 let wsServer = new WebSocketServer({
@@ -18,31 +19,31 @@ let lastColor = '{"M": "0", "R": "10", "G": "0", "B": "0"}';
 wsServer.on('request', (request) => {
     if (!clientIdIsAllowed(request.resourceURL.query["clientId"])) {
         request.reject();
-        console.log((new Date()) + ' Conexão rejeitada. Origem: ' + request.origin);
+        log('Conexão rejeitada. Origem: ' + request.origin);
         return;
     }
 
     let connection = request.accept(null, request.origin);
     connections.push(connection);
-    console.log((new Date()) + ' Conexão aceita!');
+    log('Conexão aceita!');
 
     connection.sendUTF(lastColor);
 
     connection.on('message', function (message) {
         if (message.type === 'utf8') {
             lastColor = message.utf8Data;
-            console.log((new Date()) + ' Mensagem recebida: ' + lastColor);
+            log('Mensagem recebida: ' + lastColor);
             connections.forEach((element) => { 
                 element.sendUTF(lastColor);
             });
         }
         else {
-            console.log((new Date()) + ' A mensagem deve ser enviada em UTF-8');
+            log('A mensagem deve ser enviada em UTF-8');
         }
     });
 
     connection.on('close', function (reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' desconectado.');
+        log('Peer ' + connection.remoteAddress + ' desconectado.');
         connections = connections.filter((element) => { element != connection });
     });
 });
@@ -51,4 +52,9 @@ function clientIdIsAllowed(clientId) {
     if (clientId == 0 || clientId == 1) {
         return true;
     }
+}
+
+function log(message){
+    let date = moment().format('YYYY-MM-DD hh:mm:ss.SSS: ');
+    console.log(date + message);
 }
