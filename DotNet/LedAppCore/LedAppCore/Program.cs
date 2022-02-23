38bc7a5f9b054ba45c3fc87d6d034e37dfbaf85d
GabrielCapila <gabriel.capila@sinqia.com.br>
2022-02-22 22:06:35 -0300

    Criando servidor(start e stop). Recebendo e enviando mensagem do client

--- DotNet/LedAppCore/LedAppCore/Program.cs
+++ DotNet/LedAppCore/LedAppCore/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+
+WebSocketServer server = new ("ws://127.0.0.1:5000");
+server.AddWebSocketService<Echo>("/Echo");
+server.Start();
+Console.ReadKey();
+server.Stop();
+
+public class Echo : WebSocketBehavior {
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Console.WriteLine("Mensagem recebida: " + e.Data + "do server");
+        Sessions.Broadcast(e.Data);
+    }
+}
